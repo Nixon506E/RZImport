6baee292f03bf4f496285ea60309a19999f11923
Keith Lamprecht <1894492+Nixon506E@users.noreply.github.com>
2021-09-22 16:17:34 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         .target(
             name: "RZImport",
             path: "Classes",
-            publicHeadersPath: ""),
+            publicHeadersPath: "Classes"),
     ]
 )
